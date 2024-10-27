5c953470db877b1c9d8db68e2fb575203a673915
Nikolay <kalmykov.n.s.89@gmail.com>
2024-10-27 12:24:45 +0100

    [#-0] eureka configuration

--- application/src/main/java/com/easyfleet/application/Application.java
+++ application/src/main/java/com/easyfleet/application/Application.java
@@ -2,11 +2,8 @@ package com.easyfleet.application;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.RestController;
-
 
 @SpringBootApplication(scanBasePackages = "com.easyfleet")
-@RestController
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
