9966521e6e2238ed13ac2366933e10fece46c3af
Nikolay <kalmykov.n.s.89@gmail.com>
2024-11-03 15:53:14 +0100

    [#EAS-1] Vehicle rest api

--- application/src/main/java/com/easyfleet/application/controllers/VehicleController.java
+++ application/src/main/java/com/easyfleet/application/controllers/VehicleController.java
@@ -3,6 +3,7 @@ package com.easyfleet.application.controllers;
 import com.easyfleet.api.dto.VehicleDTO;
 import com.easyfleet.application.services.IVehicleService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -13,12 +14,20 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/v1/vehicles")
 @RequiredArgsConstructor
 public class VehicleController {
+    @Value("${environment.message}")
+    private String env;
+
     private final IVehicleService service;
 
     @GetMapping("/{id}")
     public ResponseEntity<VehicleDTO> getOne(@PathVariable Long id) {
         VehicleDTO vehicleDTO = service.getOne(id);
         return ResponseEntity.ok(vehicleDTO);
+    }
+
+    @GetMapping("/ping")
+    public ResponseEntity<String> ping() {
+        return ResponseEntity.ok(env);
     }
 
 
