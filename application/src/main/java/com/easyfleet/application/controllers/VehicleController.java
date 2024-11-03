1705b720c3fefb9a603333e2d3333d5f480cae16
Nikolay <kalmykov.n.s.89@gmail.com>
2024-11-03 15:27:55 +0100

    [#EAS-1] Vehicle rest api

--- application/src/main/java/com/easyfleet/application/controllers/VehicleController.java
+++ application/src/main/java/com/easyfleet/application/controllers/VehicleController.java
@@ -1,22 +1,16 @@
 package com.easyfleet.application.controllers;
 
-import com.easyfleet.api.dto.DeviceDTO;
-import com.easyfleet.api.dto.PositionDTO;
 import com.easyfleet.api.dto.VehicleDTO;
-import com.easyfleet.api.enums.DeviceType;
 import com.easyfleet.application.services.IVehicleService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.math.BigDecimal;
-
 @RestController
-@RequestMapping("/api/v1/vehicles")
+@RequestMapping("/v1/vehicles")
 @RequiredArgsConstructor
 public class VehicleController {
     private final IVehicleService service;
@@ -24,7 +18,7 @@ public class VehicleController {
     @GetMapping("/{id}")
     public ResponseEntity<VehicleDTO> getOne(@PathVariable Long id) {
         VehicleDTO vehicleDTO = service.getOne(id);
-        return new ResponseEntity<>(vehicleDTO, HttpStatus.OK);
+        return ResponseEntity.ok(vehicleDTO);
     }
 
 
