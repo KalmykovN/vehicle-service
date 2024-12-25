d05791dabe3dc33f53178dc93cfed707607f86e4
Nikolay <kalmykov.n.s.89@gmail.com>
2024-12-25 09:53:06 +0100

    [#EAS-1] Config rabbit added

--- application/src/main/java/com/easyfleet/application/controllers/VehicleController.java
+++ application/src/main/java/com/easyfleet/application/controllers/VehicleController.java
@@ -4,18 +4,20 @@ import com.easyfleet.api.dto.VehicleDTO;
 import com.easyfleet.application.services.IVehicleService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
+import org.springframework.core.env.Environment;
+@RefreshScope
 @RestController
 @RequestMapping("/v1/vehicles")
 @RequiredArgsConstructor
 public class VehicleController {
     private final IVehicleService service;
-
+    private final Environment environment;
     @Value("${environment.message}")
     private String env;
 
